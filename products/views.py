5ef930022e444d4e58ba2d22d729d1299c7114dd
Ahsen Tekdemir <ahsentekdemir.sh@gmail.com>
2021-06-21 02:56:32 +0300

    list func

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProductSerializers
 
-# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products =  Products.objects.all()
+        serializers = ProductSerializers
+    def create(self, request):
+        pass
+    def retreive(self, request, id=None):
+        pass
+    def update(self, request, id=None):
+        pass
+    def delete(self, request, id=None):
+        pass
