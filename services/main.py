1eea883f3f14995be40a4ded78f420ea91192211
ahsen tekdemir <somethingsmeels@gmail.com>
2021-06-22 16:47:57 +0300

    add: rabbitmq consumer, producer

--- services/main.py
+++ services/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db_service/main'
 
 CORS(app)
 
