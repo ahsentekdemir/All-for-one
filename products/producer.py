5924ce0ea27bb91c463f8a20258736a94bfa7f93
ahsen tekdemir <somethingsmeels@gmail.com>
2021-06-22 16:51:14 +0300

    fix commit

--- products/producer.py
+++ products/producer.py
@@ -1,10 +1,10 @@
 import pika
 
-params = pika.URLParameters('amqps://')
+params = pika.URLParameters('amqps://test')
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 def publish(method, body):
-    channel.basic_publish(exchange='', routing_key='admin', body='test')
+    channel.basic_publish(exchange='', routing_key='admin', body='testdata')
 
 
